94b300d15b886f385edea638edd1aae7112346fe
Vincent Lee <lee.m.vincent@gmail.com>
2015-01-21 17:31:35 -0800

    Added UI to LoginVC. Created Login files

---
+++ StoryApp/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  StoryApp
+//
+//  Created by Vincent Lee on 1/21/15.
+//  Copyright (c) 2015 VincentLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ StoryApp/LoginViewModel.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.h
+//  StoryApp
+//
+//  Created by Vincent Lee on 1/21/15.
+//  Copyright (c) 2015 VincentLee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginViewModel : NSObject
+
+@end
